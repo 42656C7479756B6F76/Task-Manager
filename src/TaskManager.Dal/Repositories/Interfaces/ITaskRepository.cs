55d4326da9bfdba1df0ee95a9a009cac944c0132
Beltyukov Danil <beltyukov.danil.evgenevich@gmail.com>
2024-11-30 23:18:38 +0300

    Added repository interfaces

---
+++ src/TaskManager.Dal/Repositories/Interfaces/ITaskRepository.cs
@@ -0,0 +1,16 @@
+using TaskManager.Dal.Entities;
+using TaskManager.Dal.Models;
+using TaskManager.Dal.Models;
+
+namespace TaskManager.Dal.Repositories.Interfaces;
+
+public interface ITaskRepository
+{
+    Task<long[]> Add(TaskEntityV1[] tasks, CancellationToken token);
+    
+    Task<TaskEntityV1[]> Get(TaskGetModel query, CancellationToken token);
+
+    Task Assign(AssignTaskModel model, CancellationToken token);
+    
+    Task<SubTaskModel[]> GetSubTasksInStatus(long parentTaskId, int[] statuses, CancellationToken token);
+}
