ccd577e756c8bc3ac7ec0b99c7b8f2fac594e912
Beltyukov Danil <beltyukov.danil.evgenevich@gmail.com>
2024-12-01 14:43:19 +0300

    Added extensions for Host to create migrations

---
+++ src/TaskManager.Dal/Infrastructure/Postgres.cs
@@ -0,0 +1,30 @@
+using FluentMigrator.Runner;
+using TaskManager.Dal.Settings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Npgsql;
+using Npgsql.NameTranslation;
+
+namespace TaskManager.Dal.Infrastructure;
+
+public static class Postgres
+{
+    private static readonly INpgsqlNameTranslator Translator = new NpgsqlSnakeCaseNameTranslator();
+
+    /// <summary>
+    /// Add migration infrastructure
+    /// </summary>
+    public static void AddMigrations(IServiceCollection services)
+    {
+        services.AddFluentMigratorCore()
+            .ConfigureRunner(rb => rb.AddPostgres()
+                .WithGlobalConnectionString(s =>
+                {
+                    var cfg = s.GetRequiredService<IOptions<DalOptions>>();
+                    return cfg.Value.PostgresConnectionString;
+                })
+                .ScanIn(typeof(Postgres).Assembly).For.Migrations()
+            )
+            .AddLogging(lb => lb.AddFluentMigratorConsole());
+    }
+}
